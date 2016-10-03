99e56f94c0f5af7cd96928708920d6dd643a682d
Shaun Crampton <shaun@tigera.io>
2016-10-03 10:06:06 +0100

    Update wire-format docs.

--- go/felix/proto/doc.go
+++ go/felix/proto/doc.go
@@ -12,67 +12,77 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// The proto package defines the protocol between the front-end and back-end.
+// The proto package defines the protocol between the main, calico-felix,
+// process (written in Golang) and the dataplane driver plugin.
 //
 // Overview
 //
-// The front-end creates a server socket and accepts the connection from the
-// backend.
+// The main process creates a pair of communication pipes, before forking
+// to start the dataplane driver.  The dataplane driver receives messages on
+// file descriptor 3 and sends on descriptor 4.
 //
 // The protocol (described in more detail below) starts with a handshake to
-// exchange configuration.  Then the back-end begins its resync with the
+// exchange configuration.  Then the main process begins its resync with the
 // datastore, emitting updates as it scans through the current state.
-// Once complete, the back-end enters the "in-sync" state and starts sending
-// only updates.  The front-end may send  process/endpoint status updates at
-// any time after the handshake.
+// Once complete, the main process enters the "in-sync" state and starts
+// sending only updates.  The driver may send  process/endpoint status
+// updates at any time after the handshake.
 //
-// The wire-format for the protocol uses messagepack.  It is described in
+// The wire-format for the protocol uses protobuf.  It is described in
 // more detail below.
 //
 // Handshake
 //
-// The protocol starts with handshake messages.  The front-end sends an Init
-// message with the datastore connection config.  Then, the back-end loads
-// additional raw config from the datastore and returns it in a ConfigUpdate
-// message. Finally, the front-end parses and resolves the raw config and
-// responds with the "final" config in a ConfigResolved message.
+// Before sending its stream of updates, the main process loads and resolves
+// the configuration (from file, environment variables and the datastore) and
+// then sends a ConfigUpdate message with the resolved configuration.  This
+// ensures that the driver has the configuration before it receives any
+// updates.
+//
+// Note: the main process doesn't currently support any subsequent config
+// updates.  If the config is updated after the process is running, it will
+// exit, so that the init system can restart it.
 //
 // Resync and updates
 //
-// After the handshake, the back-end enters enters the "resync" state,
-// during which it both: scans through the complete state of the datastore,
-// and, processes update events from the datastore as they come in.  When it
-// detects an active resource, whether in the datastore scan or the stream of
-// updates from the datastore, it sends an <Type>Update message to the
-// frontend (or a <Type>Remove for a deletion).
+// After the initial ConfigUpdate message, the protocol is in resync state.
+// The main process will send a stream of updates that merges the current state
+// of the datastore along with any updates that occur later.  The stream is
+// guaranteed to be eventually consistent.  I.e. if a resource is updated (or
+// deleted) during the resync then the main process is free to skip the
+// intermediate value and send only one update with the most up-to-date value
+// (or none if the object was deleted).
 //
-// Note: if there are concurrent updates to the datastore during the resync,
-// the back-end will send an eventually-consistent stream of events.  For
-// example, if an object in the datamodel exists in the datastore at the
-// start of the resync but then it is deleted while the resync is in progress,
-// the back-end may send an update for the resource then a delete, or, it may
-// skip the resource entirely. (But it will not send a Remove then an Update
-// because that would leave the front-end in the incorrect state.)
+// Once the main process has finished its initial datastore scan, it sends the
+// InSync message.
 //
-// For simplicity and robustness, <Type>Update messages contain the complete
-// current state of the resource that they refer to.  However, for
+// For simplicity and robustness, most <Type>Update messages contain the
+// complete current state of the resource that they refer to.  However, for
 // performance, IP set updates are communicated as an initial IPSetUpdate,
 // followed by a sequence of IPSetDeltaUpdate messages.
 //
-// Once all of the pre-existing data has been sent, the back-end enters the
-// "in-sync" state and starts sending only updates.
+// Graceful restart
 //
-// If it loses sync with the datastore and has to do another scan, it
-// re-enters the "resync" state.
+// During the resync, the dataplane driver is likely to have an incomplete
+// picture of the desired state of the dataplane.  If it started programming
+// the dataplane immediately, message-by-message after a restart then it may
+// disrupt connectivity to already-configured workloads.  To prevent this,
+// the dataplane driver should delay programming of potentially incorrect
+// state until after it receives the InSync message.
+//
+// Note: there are many safe updates that the dataplane driver can make during
+// the resync.  For example, if it receives a profile update, it is free to
+// overwrite the exsting profile state because the profile update contains a
+// complete and consistent snapshot of the profile.
 //
 // Special cases
 //
-// If the back-end fails to parse an update from the datastore, it simulates
-// a deletion for the relevant resource.  As such the front-end must be robust
-// to duplicate Remove messages as well as receiving a Remove message for a
-// resource that it hadn't previously been told about.
+// If the main process fails to parse an update from the datastore, it
+// simulates a deletion for the relevant resource.  As such the driver must be
+// robust to duplicate Remove messages as well as receiving a Remove message
+// for a resource that it hadn't previously been told about.
 //
-// The above also implies that the front-end needs to be robust against
+// The above also implies that the driver needs to be robust against
 // receiving partial information.  For example,. if it receives an endpoint
 // that refers to profile X but profile X is never sent or is deleted then it
 // should handle that by dropping packets that would go to profile X.
@@ -81,130 +91,103 @@
 //
 // The protocol flow is illustrated below.
 //
-//	+-----------+                                  +---------+
-//	| frontend  |                                  | backend |
-//	+-----------+                                  +---------+
-//	      |                                             |
-//	      | **Create**                                  |
-//	      |-------------------------------------------->|
-//	      | ---------------------\                      |
-//	      |-| Start of handshake |                      |
-//	      | |--------------------|                      |
-//	      |                                             |
-//	      | Init(Hostname, etcd config)                 |
-//	      |-------------------------------------------->|
-//	      |                   ------------------------\ |
-//	      |                   | Connects to datastore |-|
-//	      |                   |-----------------------| |
-//	      |                                             |
-//	      |              ConfigUpdate(global, per-host) |
-//	      |<--------------------------------------------|
-//	      |                                             |
-//	      | ConfigResolved(logging config)              |
-//	      |-------------------------------------------->|
-//	      |                        -------------------\ |
-//	      |                        | End of handshake |-|
-//	      |                        |------------------| |
-//	      |                                             |
-//	      |           DatastoreStatus("wait-for-ready") |
-//	      |<--------------------------------------------|
-//	      |         ----------------------------------\ |
-//	      |         | Starts  resync, sending updates |-|
-//	      |         |---------------------------------| |
-//	      |                                             |
-//	      |                   DatastoreStatus("resync") |
-//	      |<--------------------------------------------|
-//	      |                                             |
-//	      |            IPSet(Update|DeltaUpdate|Remove) |
-//	      |<--------------------------------------------|
-//	      |                                             |
-//	      |       Active(Profile|Policy)(Update|Remove) |
-//	      |<--------------------------------------------|
-//	      |                                             |
-//	      |      (Workload|Host)Endpoint(Update|Remove) |
-//	      |<--------------------------------------------|
-//	      |                           ----------------\ |
-//	      |                           | Finishes sync |-|
-//	      |                           |---------------| |
-//	      |                                             |
-//	      |              ConfigUpdate(global, per-host) |
-//	      |<--------------------------------------------|
-//	      |                                             |
-//	      |                  DatastoreStatus("in-sync") |
-//	      |<--------------------------------------------|
-//	      |                                             |
-//	      |            IPSet(Update|DeltaUpdate|Remove) |
-//	      |<--------------------------------------------|
-//	      |                                             |
-//	      |       Active(Profile|Policy)(Update|Remove) |
-//	      |<--------------------------------------------|
-//	      |                                             |
-//	      |      (Workload|Host)Endpoint(Update|Remove) |
-//	      |<--------------------------------------------|
-//	      | ------------------------------------\       |
-//	      |-| Status updates (sent at any time) |       |
-//	      | |-----------------------------------|       |
-//	      |                                             |
-//	      | FelixStatusUpdate                           |
-//	      |-------------------------------------------->|
-//	      |                                             |
-//	      | (Workload|Host)EndpointStatus               |
-//	      |-------------------------------------------->|
-//	      |                                             |
+//	+-----------+                                             +-------+
+//	| dp_driver |                                             | main  |
+//	+-----------+                                             +-------+
+//	      |                                                       |
+//	      |                                            **Create** |
+//	      |<------------------------------------------------------|
+//	      |               --------------------------------------\ |
+//	      |               | Connects to datastore, loads config |-|
+//	      |               |-------------------------------------| |
+//	      |                                                       |
+//	      |                         ConfigUpdate(resolved config) |
+//	      |<------------------------------------------------------|
+//	      | --------------------------------------------------\   |
+//	      |-| Start graceful restart, avoid removing DP state |   |
+//	      | |-------------------------------------------------|   |
+//	      |                                                       |
+//	      |                     DatastoreStatus("wait-for-ready") |
+//	      |<------------------------------------------------------|
+//	      |                   ----------------------------------\ |
+//	      |                   | Starts  resync, sending updates |-|
+//	      |                   |---------------------------------| |
+//	      |                                                       |
+//	      |                             DatastoreStatus("resync") |
+//	      |<------------------------------------------------------|
+//	      |                                                       |
+//	      |                      IPSet(Update|DeltaUpdate|Remove) |
+//	      |<------------------------------------------------------|
+//	      |                                                       |
+//	      |                 Active(Profile|Policy)(Update|Remove) |
+//	      |<------------------------------------------------------|
+//	      |                                                       |
+//	      |                (Workload|Host)Endpoint(Update|Remove) |
+//	      |<------------------------------------------------------|
+//	      |                                     ----------------\ |
+//	      |                                     | Finishes sync |-|
+//	      |                                     |---------------| |
+//	      |                                                       |
+//	      |                            DatastoreStatus("in-sync") |
+//	      |<------------------------------------------------------|
+//	      | -----------------------------------------\            |
+//	      |-| Finish graceful restart, do DP cleanup |            |
+//	      | |----------------------------------------|            |
+//	      |                                                       |
+//	      |                      IPSet(Update|DeltaUpdate|Remove) |
+//	      |<------------------------------------------------------|
+//	      |                                                       |
+//	      |                 Active(Profile|Policy)(Update|Remove) |
+//	      |<------------------------------------------------------|
+//	      |                                                       |
+//	      |                (Workload|Host)Endpoint(Update|Remove) |
+//	      |<------------------------------------------------------|
+//	      | ------------------------------------\                 |
+//	      |-| Status updates (sent at any time) |                 |
+//	      | |-----------------------------------|                 |
+//	      |                                                       |
+//	      | FelixStatusUpdate                                     |
+//	      |------------------------------------------------------>|
+//	      |                                                       |
+//	      | (Workload|Host)EndpointStatus                         |
+//	      |------------------------------------------------------>|
+//	      |                                                       |
 //
 // Wire format
 //
-// The protocol between the front-end and back-end is message-pack based.
-// On the wire, each message consists fo two messagepack objects.  The first
-// is a string identifying the type of message.  The second is the body of the
-// message, which is a dict.
-//
-// To simplify encoding/decoding messages into structs, this package provides
-// the Envelope struct.  The Envelope struct contains a Payload field to hold
-// a pointer to one of the message body structs.  When marshaled, it maps the
-// type of the payload to the correct type name and vice-versa:
-//
-// 	// Encoding:
-// 	msg := DatastoreStatus{Status: "resync"}
-// 	envelope := Envelope{Payload:&msg}
-// 	encoder.Encode(envelope)
-// 	... encodes "datastore_status" {"status": "resync"}
-//
-// 	// Decoding:
-// 	envelope := Envelope{}
-// 	decoder.decode(&envelope)
-// 	... envelope.Payload is now a DatastoreStatus{}
+// The protocol between the driver and main process is protobuf based.
+// On the wire, each message consists of an 8-byte, little-endian length,
+// followed by a ToDataplane or FromDataplane protobuf envelope message.
+// The length refers to the length of the protobuf data only, it doesn't
+// include the 8-byte length header.
 package proto
 
 // http://textart.io/sequence Source code for sequence diagram above:
 
 var _ = `
-object frontend backend
-frontend->backend: **Create**
-note right of frontend: Start of handshake
-frontend->backend: Init(Hostname, etcd config)
-note left of backend: Connects to datastore
-backend->frontend: ConfigUpdate(global, per-host)
-frontend->backend: ConfigResolved(logging config)
-note left of backend: End of handshake
+object dp_driver main
+main->dp_driver: **Create**
+note left of main: Connects to datastore, loads config
+main->dp_driver: ConfigUpdate(resolved config)
+note right of dp_driver: Start graceful restart, avoid removing DP state
 
-backend->frontend: DatastoreStatus("wait-for-ready")
-note left of backend: Starts  resync, sending updates
-backend->frontend: DatastoreStatus("resync")
+main->dp_driver: DatastoreStatus("wait-for-ready")
+note left of main: Starts  resync, sending updates
+main->dp_driver: DatastoreStatus("resync")
 
-backend->frontend: IPSet(Update|DeltaUpdate|Remove)
-backend->frontend: Active(Profile|Policy)(Update|Remove)
-backend->frontend: (Workload|Host)Endpoint(Update|Remove)
+main->dp_driver: IPSet(Update|DeltaUpdate|Remove)
+main->dp_driver: Active(Profile|Policy)(Update|Remove)
+main->dp_driver: (Workload|Host)Endpoint(Update|Remove)
 
-note left of backend: Finishes sync
-backend->frontend: ConfigUpdate(global, per-host)
-backend->frontend: DatastoreStatus("in-sync")
+note left of main: Finishes sync
+main->dp_driver: DatastoreStatus("in-sync")
+note right of dp_driver: Finish graceful restart, do DP cleanup
 
-backend->frontend: IPSet(Update|DeltaUpdate|Remove)
-backend->frontend: Active(Profile|Policy)(Update|Remove)
-backend->frontend: (Workload|Host)Endpoint(Update|Remove)
+main->dp_driver: IPSet(Update|DeltaUpdate|Remove)
+main->dp_driver: Active(Profile|Policy)(Update|Remove)
+main->dp_driver: (Workload|Host)Endpoint(Update|Remove)
 
-note right of frontend: Status updates (sent at any time)
-frontend->backend: FelixStatusUpdate
-frontend->backend: (Workload|Host)EndpointStatus
+note right of dp_driver: Status updates (sent at any time)
+dp_driver->main: FelixStatusUpdate
+dp_driver->main: (Workload|Host)EndpointStatus
 `
