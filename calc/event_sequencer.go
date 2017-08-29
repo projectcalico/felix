0a50ae89afb2c61c91694f47cfec308650d687b9
song <song@tigera.io>
2017-08-29 16:58:34 +0100

    Added support for ApplyOnForward in calc graph.

--- calc/event_sequencer.go
+++ calc/event_sequencer.go
@@ -322,7 +322,7 @@ func ModelWorkloadEndpointToProto(ep *model.WorkloadEndpoint, tiers []*proto.Tie
 	}
 }
 
-func ModelHostEndpointToProto(ep *model.HostEndpoint, tiers, untrackedTiers, preDNATTiers []*proto.TierInfo) *proto.HostEndpoint {
+func ModelHostEndpointToProto(ep *model.HostEndpoint, tiers, untrackedTiers, preDNATTiers []*proto.TierInfo, forwardTiers []*proto.TierInfo) *proto.HostEndpoint {
 	return &proto.HostEndpoint{
 		Name:              ep.Name,
 		ExpectedIpv4Addrs: ipsToStrings(ep.ExpectedIPv4Addrs),
@@ -331,6 +331,7 @@ func ModelHostEndpointToProto(ep *model.HostEndpoint, tiers, untrackedTiers, pre
 		Tiers:             tiers,
 		UntrackedTiers:    untrackedTiers,
 		PreDnatTiers:      preDNATTiers,
+		ForwardTiers:      forwardTiers,
 	}
 }
 
@@ -356,7 +357,7 @@ func (buf *EventSequencer) OnEndpointTierUpdate(key model.Key,
 
 func (buf *EventSequencer) flushEndpointTierUpdates() {
 	for key, endpoint := range buf.pendingEndpointUpdates {
-		tiers, untrackedTiers, preDNATTiers := tierInfoToProtoTierInfo(buf.pendingEndpointTierUpdates[key])
+		tiers, untrackedTiers, preDNATTiers, forwardTiers := tierInfoToProtoTierInfo(buf.pendingEndpointTierUpdates[key])
 		switch key := key.(type) {
 		case model.WorkloadEndpointKey:
 			wlep := endpoint.(*model.WorkloadEndpoint)
@@ -374,7 +375,7 @@ func (buf *EventSequencer) flushEndpointTierUpdates() {
 				Id: &proto.HostEndpointID{
 					EndpointId: key.EndpointID,
 				},
-				Endpoint: ModelHostEndpointToProto(hep, tiers, untrackedTiers, preDNATTiers),
+				Endpoint: ModelHostEndpointToProto(hep, tiers, untrackedTiers, preDNATTiers, forwardTiers),
 			})
 		}
 		// Record that we've sent this endpoint.
@@ -587,30 +588,34 @@ func addPolicyToTierInfo(pol *PolKV, tierInfo *proto.TierInfo, egressAllowed boo
 	}
 }
 
-func tierInfoToProtoTierInfo(filteredTiers []tierInfo) (normalTiers, untrackedTiers, preDNATTiers []*proto.TierInfo) {
+func tierInfoToProtoTierInfo(filteredTiers []tierInfo) (normalTiers, untrackedTiers, preDNATTiers, forwardTiers []*proto.TierInfo) {
 	if len(filteredTiers) > 0 {
+		checkAndAddToTiers := func(tiers []*proto.TierInfo, tierInfo *proto.TierInfo) []*proto.TierInfo {
+			if len(tierInfo.IngressPolicies) > 0 || len(tierInfo.EgressPolicies) > 0 {
+				return append(tiers, tierInfo)
+			}
+			return tiers
+		}
 		for _, ti := range filteredTiers {
 			untrackedTierInfo := &proto.TierInfo{Name: ti.Name}
 			preDNATTierInfo := &proto.TierInfo{Name: ti.Name}
+			forwardTierInfo := &proto.TierInfo{Name: ti.Name}
 			normalTierInfo := &proto.TierInfo{Name: ti.Name}
 			for _, pol := range ti.OrderedPolicies {
 				if pol.Value.DoNotTrack {
 					addPolicyToTierInfo(&pol, untrackedTierInfo, true)
 				} else if pol.Value.PreDNAT {
 					addPolicyToTierInfo(&pol, preDNATTierInfo, false)
+				} else if *pol.Value.ApplyOnForward {
+					addPolicyToTierInfo(&pol, forwardTierInfo, true)
 				} else {
 					addPolicyToTierInfo(&pol, normalTierInfo, true)
 				}
 			}
-			if len(untrackedTierInfo.IngressPolicies) > 0 || len(untrackedTierInfo.EgressPolicies) > 0 {
-				untrackedTiers = append(untrackedTiers, untrackedTierInfo)
-			}
-			if len(preDNATTierInfo.IngressPolicies) > 0 || len(preDNATTierInfo.EgressPolicies) > 0 {
-				preDNATTiers = append(preDNATTiers, preDNATTierInfo)
-			}
-			if len(normalTierInfo.IngressPolicies) > 0 || len(normalTierInfo.EgressPolicies) > 0 {
-				normalTiers = append(normalTiers, normalTierInfo)
-			}
+			checkAndAddToTiers(forwardTiers, forwardTierInfo)
+			checkAndAddToTiers(untrackedTiers, untrackedTierInfo)
+			checkAndAddToTiers(preDNATTiers, preDNATTierInfo)
+			checkAndAddToTiers(normalTiers, normalTierInfo)
 		}
 	}
 	return
