a05c7b43e7891643eeb345b58019fbd9788d9303
Rafael Vanoni <rafael@tigera.io>
2019-06-25 01:23:53 -0700

    Drop IPIP packets originating in pods

--- dataplane/linux/endpoint_mgr_test.go
+++ dataplane/linux/endpoint_mgr_test.go
@@ -135,11 +135,17 @@ func chainsForIfaces(ifaceMetadata []string,
 	epMarkMapper rules.EndpointMarkMapper,
 	host bool,
 	tableKind string) []*iptables.Chain {
+	const (
+		ProtoUDP  = 17
+		ProtoIPIP = 4
+	)
+
 	log.WithFields(log.Fields{
 		"ifaces":    ifaceMetadata,
 		"host":      host,
 		"tableKind": tableKind,
 	}).Debug("Calculating chains for interface")
+
 	chains := []*iptables.Chain{}
 	dispatchOut := []iptables.Rule{}
 	dispatchIn := []iptables.Rule{}
@@ -153,6 +159,13 @@ func chainsForIfaces(ifaceMetadata []string,
 	epMarkFromName := "cali-from-endpoint-mark"
 	epMarkSetOnePrefix := "cali-sm-"
 	epmarkFromPrefix := outPrefix[:6]
+	dropEncapRules := []iptables.Rule{
+		{
+			Match:   iptables.Match().ProtocolNum(ProtoIPIP),
+			Action:  iptables.DropAction{},
+			Comment: "Drop IPinIP encapped packets originating in pods",
+		},
+	}
 
 	if host {
 		hostOrWlLetter = "h"
@@ -235,6 +248,8 @@ func chainsForIfaces(ifaceMetadata []string,
 			Match:  iptables.Match(),
 			Action: iptables.ClearMarkAction{Mark: 8},
 		})
+		outRules = append(outRules, dropEncapRules...)
+
 		if egress && polName != "" && tableKind == ifaceKind {
 			outRules = append(outRules, iptables.Rule{
 				Match:   iptables.Match(),
@@ -313,6 +328,8 @@ func chainsForIfaces(ifaceMetadata []string,
 			Match:  iptables.Match(),
 			Action: iptables.ClearMarkAction{Mark: 8},
 		})
+		inRules = append(inRules, dropEncapRules...)
+
 		if ingress && polName != "" && tableKind == ifaceKind {
 			inRules = append(inRules, iptables.Rule{
 				Match:   iptables.Match(),
--- rules/endpoints_test.go
+++ rules/endpoints_test.go
@@ -27,6 +27,11 @@ import (
 )
 
 var _ = Describe("Endpoints", func() {
+	const (
+		ProtoUDP  = 17
+		ProtoIPIP = 4
+	)
+
 	for _, trueOrFalse := range []bool{true, false} {
 		kubeIPVSEnabled := trueOrFalse
 		var rrConfigNormalMangleReturn = Config{
@@ -63,6 +68,12 @@ var _ = Describe("Endpoints", func() {
 		var renderer RuleRenderer
 		var epMarkMapper EndpointMarkMapper
 
+		dropIPIPRule := Rule{
+			Match:   Match().ProtocolNum(ProtoIPIP),
+			Action:  DropAction{},
+			Comment: "Drop IPinIP encapped packets originating in pods",
+		}
+
 		Context("with normal config", func() {
 			BeforeEach(func() {
 				renderer = NewRenderer(rrConfigNormalMangleReturn)
@@ -70,7 +81,7 @@ var _ = Describe("Endpoints", func() {
 					rrConfigNormalMangleReturn.IptablesMarkNonCaliEndpoint)
 			})
 
-			It("Song should render a minimal workload endpoint", func() {
+			It("should render a minimal workload endpoint", func() {
 				Expect(renderer.WorkloadEndpointToIptablesChains(
 					"cali1234", epMarkMapper,
 					true,
@@ -87,6 +98,7 @@ var _ = Describe("Endpoints", func() {
 								Action: DropAction{}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
 							{Action: DropAction{},
 								Comment: "Drop if no profiles matched"},
 						},
@@ -101,6 +113,7 @@ var _ = Describe("Endpoints", func() {
 								Action: DropAction{}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
 							{Action: DropAction{},
 								Comment: "Drop if no profiles matched"},
 						},
@@ -164,6 +177,7 @@ var _ = Describe("Endpoints", func() {
 								Action: DropAction{}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
 
 							{Comment: "Start of policies",
 								Action: ClearMarkAction{Mark: 0x10}},
@@ -204,6 +218,7 @@ var _ = Describe("Endpoints", func() {
 								Action: DropAction{}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
 
 							{Comment: "Start of policies",
 								Action: ClearMarkAction{Mark: 0x10}},
@@ -262,6 +277,7 @@ var _ = Describe("Endpoints", func() {
 							{Action: JumpAction{Target: "cali-failsafe-out"}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
 
 							{Comment: "Start of policies",
 								Action: ClearMarkAction{Mark: 0x10}},
@@ -305,6 +321,7 @@ var _ = Describe("Endpoints", func() {
 							{Action: JumpAction{Target: "cali-failsafe-in"}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
 
 							{Comment: "Start of policies",
 								Action: ClearMarkAction{Mark: 0x10}},
@@ -345,6 +362,7 @@ var _ = Describe("Endpoints", func() {
 								Action: DropAction{}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
 
 							{Comment: "Start of policies",
 								Action: ClearMarkAction{Mark: 0x10}},
@@ -373,6 +391,7 @@ var _ = Describe("Endpoints", func() {
 								Action: DropAction{}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
 
 							{Comment: "Start of policies",
 								Action: ClearMarkAction{Mark: 0x10}},
@@ -409,6 +428,7 @@ var _ = Describe("Endpoints", func() {
 							{Action: JumpAction{Target: "cali-failsafe-out"}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
 
 							{Comment: "Start of policies",
 								Action: ClearMarkAction{Mark: 0x10}},
@@ -431,6 +451,7 @@ var _ = Describe("Endpoints", func() {
 							{Action: JumpAction{Target: "cali-failsafe-in"}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
 
 							{Comment: "Start of policies",
 								Action: ClearMarkAction{Mark: 0x10}},
@@ -469,6 +490,7 @@ var _ = Describe("Endpoints", func() {
 							{Action: JumpAction{Target: "cali-failsafe-in"}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
 
 							{Comment: "Start of policies",
 								Action: ClearMarkAction{Mark: 0x10}},
@@ -511,6 +533,8 @@ var _ = Describe("Endpoints", func() {
 								Action: ReturnAction{}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
+
 							{Action: DropAction{},
 								Comment: "Drop if no profiles matched"},
 						},
@@ -525,6 +549,8 @@ var _ = Describe("Endpoints", func() {
 								Action: ReturnAction{}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
+
 							{Action: DropAction{},
 								Comment: "Drop if no profiles matched"},
 						},
@@ -554,6 +580,7 @@ var _ = Describe("Endpoints", func() {
 							{Action: JumpAction{Target: "cali-failsafe-in"}},
 
 							{Action: ClearMarkAction{Mark: 0x8}},
+							dropIPIPRule,
 
 							{Comment: "Start of policies",
 								Action: ClearMarkAction{Mark: 0x10}},
