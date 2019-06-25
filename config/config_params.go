008ef834ba8d58bc5ab53bd907f7ab449b8974e5
Casey Davenport <casey@tigera.io>
2019-06-25 14:52:40 -0700

    Disallow enabling of XDP features

--- config/config_params.go
+++ config/config_params.go
@@ -225,9 +225,9 @@ type Config struct {
 
 	IptablesNATOutgoingInterfaceFilter string `config:"iface-param;"`
 
-	SidecarAccelerationEnabled bool `config:"bool;false"`
-	XDPEnabled                 bool `config:"bool;false"`
-	GenericXDPEnabled          bool `config:"bool;false"`
+	// SidecarAccelerationEnabled bool `config:"bool;false"`
+	// XDPEnabled                 bool `config:"bool;false"`
+	// GenericXDPEnabled          bool `config:"bool;false"`
 }
 
 type ProtoPort struct {
