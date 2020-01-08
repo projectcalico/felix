39537135cc65d0b6ce307d7bd0f87e5f11701bc6
Laurence Man <laurence@tigera.io>
2020-01-08 14:13:35 -0800

    debug

--- fv/spoof_test.go
+++ fv/spoof_test.go
@@ -35,13 +35,90 @@ var _ = infrastructure.DatastoreDescribe("spoof tests", []apiconfig.DatastoreTyp
 		cc      *workload.ConnectivityChecker
 	)
 
-	Context("using IPv4", func() {
+	/*
+		Context("using IPv4", func() {
+			BeforeEach(func() {
+				infra = getInfra()
+
+				// Setup 3 felixes. felixes[0] will spoof felixes[2] and try to reach
+				// felixes[1].
+				felixes, _ = infrastructure.StartNNodeTopology(3, infrastructure.DefaultTopologyOptions(), infra)
+
+				// Install a default profile that allows all ingress and egress, in the absence of any Policy.
+				infra.AddDefaultAllow()
+			})
+
+			AfterEach(func() {
+				if CurrentGinkgoTestDescription().Failed {
+					for _, felix := range felixes {
+						felix.Exec("iptables-save", "-c")
+						felix.Exec("ipset", "list")
+						felix.Exec("ip", "r")
+						felix.Exec("ip", "a")
+					}
+				}
+
+				for _, wl := range w {
+					wl.Stop()
+				}
+				for _, felix := range felixes {
+					felix.Stop()
+				}
+
+				if CurrentGinkgoTestDescription().Failed {
+					infra.DumpErrorData()
+				}
+				infra.Stop()
+			})
+
+			setupWorkloadsAndConnectivityChecker := func(protocol string) {
+				for ii := range w {
+					wIP := fmt.Sprintf("10.65.%d.2", ii)
+					wName := fmt.Sprintf("w%d", ii)
+					w[ii] = workload.Run(felixes[ii], wName, "default", wIP, "8055", protocol)
+					w[ii].ConfigureInDatastore(infra)
+				}
+
+				cc = &workload.ConnectivityChecker{Protocol: protocol}
+			}
+
+			It("should drop udp traffic that has had its IP spoofed", func() {
+				setupWorkloadsAndConnectivityChecker("udp")
+				// Setup a spoofed workload. w[0] has the IP 10.65.0.2.
+				// Make it use 10.65.2.2 to test connections instead.
+				spoofed := &workload.SpoofedWorkload{
+					Workload:        w[0],
+					SpoofedSourceIP: "10.65.2.2",
+				}
+				cc.ExpectNone(spoofed, w[1])
+				cc.ExpectSome(w[1], w[0])
+				cc.CheckConnectivity()
+			})
+
+			It("should drop tcp traffic that has had its IP spoofed", func() {
+				setupWorkloadsAndConnectivityChecker("tcp")
+				// Setup a spoofed workload. w[0] has the IP 10.65.0.2.
+				// Make it use 10.65.2.2 to test connections instead.
+				spoofed := &workload.SpoofedWorkload{
+					Workload:        w[0],
+					SpoofedSourceIP: "10.65.2.2",
+				}
+				cc.ExpectNone(spoofed, w[1])
+				cc.ExpectSome(w[1], w[0])
+				cc.CheckConnectivity()
+			})
+		})
+	*/
+
+	Context("using IPv6", func() {
 		BeforeEach(func() {
 			infra = getInfra()
 
 			// Setup 3 felixes. felixes[0] will spoof felixes[2] and try to reach
 			// felixes[1].
-			felixes, _ = infrastructure.StartNNodeTopology(3, infrastructure.DefaultTopologyOptions(), infra)
+			opts := infrastructure.DefaultTopologyOptions()
+			opts.EnableIPv6 = true
+			felixes, _ = infrastructure.StartNNodeTopology(3, opts, infra)
 
 			// Install a default profile that allows all ingress and egress, in the absence of any Policy.
 			infra.AddDefaultAllow()
@@ -51,6 +128,7 @@ var _ = infrastructure.DatastoreDescribe("spoof tests", []apiconfig.DatastoreTyp
 			if CurrentGinkgoTestDescription().Failed {
 				for _, felix := range felixes {
 					felix.Exec("iptables-save", "-c")
+					felix.Exec("ip6tables-save", "-c")
 					felix.Exec("ipset", "list")
 					felix.Exec("ip", "r")
 					felix.Exec("ip", "a")
@@ -71,38 +149,47 @@ var _ = infrastructure.DatastoreDescribe("spoof tests", []apiconfig.DatastoreTyp
 		})
 
 		setupWorkloadsAndConnectivityChecker := func(protocol string) {
+			// Setup an offset so that we assign IPv6 addrs like so:
+			// 0:0:0:0:0:ffff:a41:2
+			// 0:0:0:0:0:ffff:a41:102
+			// 0:0:0:0:0:ffff:a41:202
+			// ...
+			offset := 2
 			for ii := range w {
-				wIP := fmt.Sprintf("10.65.%d.2", ii)
+				wIP := fmt.Sprintf("0:0:0:0:0:ffff:a41:%d", offset)
 				wName := fmt.Sprintf("w%d", ii)
 				w[ii] = workload.Run(felixes[ii], wName, "default", wIP, "8055", protocol)
 				w[ii].ConfigureInDatastore(infra)
+				offset += 100
 			}
 
 			cc = &workload.ConnectivityChecker{Protocol: protocol}
 		}
 
-		It("should drop udp traffic that has had its IP spoofed", func() {
-			setupWorkloadsAndConnectivityChecker("udp")
-			// Setup a spoofed workload. w[0] has the IP 10.65.0.2.
-			// Make it use 10.65.2.2 to test connections instead.
-			spoofed := &workload.SpoofedWorkload{
-				Workload:        w[0],
-				SpoofedSourceIP: "10.65.2.2",
-			}
-			cc.ExpectNone(spoofed, w[1])
-			cc.ExpectSome(w[1], w[0])
-			cc.CheckConnectivity()
-		})
+		/*
+			It("should drop udp traffic that has had its IP spoofed", func() {
+				setupWorkloadsAndConnectivityChecker("udp")
+				// Setup a spoofed workload. w[0] has the IP 0:0:0:0:0:ffff:a41:2.
+				// Make it use 0:0:0:0:0:ffff:a41:202 to test connections instead.
+				spoofed := &workload.SpoofedWorkload{
+					Workload:        w[0],
+					SpoofedSourceIP: "0:0:0:0:0:ffff:a41:202",
+				}
+				cc.ExpectNone(spoofed, w[1])
+				cc.ExpectSome(w[1], w[0])
+				cc.CheckConnectivity()
+			})
+		*/
 
 		It("should drop tcp traffic that has had its IP spoofed", func() {
 			setupWorkloadsAndConnectivityChecker("tcp")
-			// Setup a spoofed workload. w[0] has the IP 10.65.0.2.
-			// Make it use 10.65.2.2 to test connections instead.
-			spoofed := &workload.SpoofedWorkload{
-				Workload:        w[0],
-				SpoofedSourceIP: "10.65.2.2",
-			}
-			cc.ExpectNone(spoofed, w[1])
+			// Setup a spoofed workload. w[0] has the IP 0:0:0:0:0:ffff:a41:2.
+			// Make it use 0:0:0:0:0:ffff:a41:202 to test connections instead.
+			//spoofed := &workload.SpoofedWorkload{
+			//	Workload:        w[0],
+			//	SpoofedSourceIP: "0:0:0:0:0:ffff:a41:202",
+			//}
+			//cc.ExpectNone(spoofed, w[1])
 			cc.ExpectSome(w[1], w[0])
 			cc.CheckConnectivity()
 		})
