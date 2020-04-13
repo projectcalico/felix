176cfbf037e5f8938b3b9d4a56ab025d525a11d9
Casey Davenport <davenport.cas@gmail.com>
2020-04-13 14:35:44 -0700

    Add Config option to set source for routing decisions

--- calc/l3_route_resolver.go
+++ calc/l3_route_resolver.go
@@ -64,6 +64,7 @@ type L3RouteResolver struct {
 	allPools               map[string]model.IPPool
 	workloadIDToCIDRs      map[model.WorkloadEndpointKey][]cnet.IPNet
 	useNodeResourceUpdates bool
+	routeSource            string
 }
 
 type l3rrNodeInfo struct {
@@ -75,7 +76,7 @@ func (i l3rrNodeInfo) AddrAsCIDR() ip.V4CIDR {
 	return i.Addr.AsCIDR().(ip.V4CIDR)
 }
 
-func NewL3RouteResolver(hostname string, callbacks PipelineCallbacks, useNodeResourceUpdates bool) *L3RouteResolver {
+func NewL3RouteResolver(hostname string, callbacks PipelineCallbacks, useNodeResourceUpdates bool, routeSource string) *L3RouteResolver {
 	logrus.Info("Creating L3 route resolver")
 	return &L3RouteResolver{
 		myNodeName: hostname,
@@ -88,6 +89,7 @@ func NewL3RouteResolver(hostname string, callbacks PipelineCallbacks, useNodeRes
 		allPools:               map[string]model.IPPool{},
 		workloadIDToCIDRs:      map[model.WorkloadEndpointKey][]cnet.IPNet{},
 		useNodeResourceUpdates: useNodeResourceUpdates,
+		routeSource:            routeSource,
 	}
 }
 
@@ -100,16 +102,17 @@ func (c *L3RouteResolver) RegisterWith(allUpdDispatcher, localDispatcher *dispat
 		allUpdDispatcher.Register(model.HostIPKey{}, c.OnHostIPUpdate)
 	}
 
-	// TODO: Uncomment this.
-	//allUpdDispatcher.Register(model.BlockKey{}, c.OnBlockUpdate)
 	allUpdDispatcher.Register(model.IPPoolKey{}, c.OnPoolUpdate)
 
 	// Depending on if we're using workload endpoints for routing information, we may
 	// need all WEPs, or only local WEPs.
-	routeSourceWEPs := true
-	if routeSourceWEPs {
+	logrus.WithField("routeSource", c.routeSource).Info("Registering for L3 route updates")
+	if c.routeSource == "workloadIPs" {
+		// Driven off of workload IP addressess. Register for all WEP udpates.
 		allUpdDispatcher.Register(model.WorkloadEndpointKey{}, c.OnWorkloadUpdate)
 	} else {
+		// Driven off of IPAM data. Register for blocks and local WEP updates.
+		allUpdDispatcher.Register(model.BlockKey{}, c.OnBlockUpdate)
 		localDispatcher.Register(model.WorkloadEndpointKey{}, c.OnWorkloadUpdate)
 	}
 }
@@ -515,7 +518,7 @@ func (c *L3RouteResolver) flush() {
 			if ri.Block.NodeName != "" {
 				rt.DstNodeName = ri.Block.NodeName
 				if rt.DstNodeName == c.myNodeName {
-					logCxt.Debug(" workload route.")
+					logCxt.Debug("Local workload route.")
 					rt.Type = proto.RouteType_LOCAL_WORKLOAD
 				} else {
 					logCxt.Debug("Remote workload route.")
@@ -526,7 +529,7 @@ func (c *L3RouteResolver) flush() {
 				rt.DstNodeName = ri.Host.NodeNames[0]
 
 				if rt.DstNodeName == c.myNodeName {
-					logCxt.Debug(" host route.")
+					logCxt.Debug("Local host route.")
 					rt.Type = proto.RouteType_LOCAL_HOST
 				} else {
 					logCxt.Debug("Remote host route.")
@@ -536,7 +539,7 @@ func (c *L3RouteResolver) flush() {
 
 			for nodename, _ := range ri.WEP.RefCount {
 				// At least one WEP exists with this IP. It may be on this node, or a remote node.
-				// In steady state we only ever expect a single WEP for this CIDR. However there are rare
+				// In steady state we only ever expect a single WEP for this CIDR. However there are rare transient
 				// cases we must handle where we may have two WEPs with the same IP. Since this will be transient,
 				// we can always just use one entry from the map.
 				rt.DstNodeName = nodename
@@ -735,7 +738,7 @@ func (r *RouteTrie) SetRouteSent(cidr ip.V4CIDR, sent bool) {
 func (r RouteTrie) updateCIDR(cidr ip.V4CIDR, updateFn func(info *RouteInfo)) bool {
 	// Get the RouteInfo for the given CIDR and take a copy so we can compare.
 	ri := r.Get(cidr)
-	riCopy := ri
+	riCopy := ri.Copy()
 
 	// Apply the update, whatever that is.
 	updateFn(&ri)
@@ -808,6 +811,17 @@ func (r RouteInfo) IsValidRoute() bool {
 		len(r.Host.NodeNames) > 0 ||
 		r.Pool.NATOutgoing ||
 		len(r.WEP.RefCount) > 0
+}
+
+// Copy returns a copy of the RouteInfo. Since some fields are pointers, we need to
+// explicitly copy them so that they are not shared between the copies.
+func (r RouteInfo) Copy() RouteInfo {
+	cp := r
+	riCopy.WEP.RefCount = map[string]int{}
+	for n, c := range ri.WEP.RefCount {
+		riCopy.WEP.RefCount[n] = c
+	}
+	return cp
 }
 
 // IsZero() returns true if this node in the trie now contains no tracking information at all and is
