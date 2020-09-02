54b342a2269bd42c310265632b2c11e77269ddde
Karthik Ramasubramanian <karthik@tigera.io>
2020-09-02 15:37:19 -0700

    Remove params that are now there in libcalico-go

--- config/config_params_test.go
+++ config/config_params_test.go
@@ -61,10 +61,6 @@ var _ = Describe("FelixConfig vs ConfigParams parity", func() {
 		"loadClientConfigFromEnvironment",
 		"useNodeResourceUpdates",
 		"internalOverrides",
-
-		// TODO(doublek): Add these two new params to libcalico-go
-		"AllowVXLANPacketsFromWorkloads",
-		"AllowIPIPPacketsFromWorkloads",
 	}
 	cpFieldNameToFC := map[string]string{
 		"IpInIpEnabled":                      "IPIPEnabled",
