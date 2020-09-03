d7049f8ffe10df22035f0a44c6fe03c1cc9cd236
Rob Brockbank <rob@tigera.io>
2020-09-02 21:03:44 -0700

    Separate node route table and endpoint is an allowed IP

--- wireguard/config.go
+++ wireguard/config.go
@@ -2,11 +2,12 @@ package wireguard
 
 type Config struct {
 	// Wireguard configuration
-	Enabled             bool
-	ListeningPort       int
-	FirewallMark        int
-	RoutingRulePriority int
-	RoutingTableIndex   int
-	InterfaceName       string
-	MTU                 int
+	Enabled                   bool
+	ListeningPort             int
+	FirewallMark              int
+	RoutingRulePriority       int
+	NodeRoutingTableIndex     int
+	WorkloadRoutingTableIndex int
+	InterfaceName             string
+	MTU                       int
 }
