db8a6d67c17d3905f822af474de3fb46d8a92f86
Shaun Crampton <shaun@tigera.io>
2020-12-01 14:23:10 +0000

    Vendor in kernel headers and tweak to compile.

--- bpf-gpl/include/linux/icmp.h
+++ bpf-gpl/include/linux/icmp.h
@@ -13,13 +13,13 @@
 #ifndef _LINUX_ICMP_H
 #define	_LINUX_ICMP_H
 
-#include <linux/skbuff.h>
+//#include <linux/skbuff.h>
 #include <uapi/linux/icmp.h>
 
-static inline struct icmphdr *icmp_hdr(const struct sk_buff *skb)
-{
-	return (struct icmphdr *)skb_transport_header(skb);
-}
+//static inline struct icmphdr *icmp_hdr(const struct sk_buff *skb)
+//{
+//	return (struct icmphdr *)skb_transport_header(skb);
+//}
 
 static inline bool icmp_is_err(int type)
 {
--- bpf-gpl/include/linux/if_ether.h
+++ bpf-gpl/include/linux/if_ether.h
@@ -16,29 +16,29 @@
 #ifndef _LINUX_IF_ETHER_H
 #define _LINUX_IF_ETHER_H
 
-#include <linux/skbuff.h>
+//#include <linux/skbuff.h>
 #include <uapi/linux/if_ether.h>
 
-static inline struct ethhdr *eth_hdr(const struct sk_buff *skb)
-{
-	return (struct ethhdr *)skb_mac_header(skb);
-}
-
-/* Prefer this version in TX path, instead of
- * skb_reset_mac_header() + eth_hdr()
- */
-static inline struct ethhdr *skb_eth_hdr(const struct sk_buff *skb)
-{
-	return (struct ethhdr *)skb->data;
-}
-
-static inline struct ethhdr *inner_eth_hdr(const struct sk_buff *skb)
-{
-	return (struct ethhdr *)skb_inner_mac_header(skb);
-}
-
-int eth_header_parse(const struct sk_buff *skb, unsigned char *haddr);
-
-extern ssize_t sysfs_format_mac(char *buf, const unsigned char *addr, int len);
+//static inline struct ethhdr *eth_hdr(const struct sk_buff *skb)
+//{
+//	return (struct ethhdr *)skb_mac_header(skb);
+//}
+//
+///* Prefer this version in TX path, instead of
+// * skb_reset_mac_header() + eth_hdr()
+// */
+//static inline struct ethhdr *skb_eth_hdr(const struct sk_buff *skb)
+//{
+//	return (struct ethhdr *)skb->data;
+//}
+//
+//static inline struct ethhdr *inner_eth_hdr(const struct sk_buff *skb)
+//{
+//	return (struct ethhdr *)skb_inner_mac_header(skb);
+//}
+//
+//int eth_header_parse(const struct sk_buff *skb, unsigned char *haddr);
+//
+//extern ssize_t sysfs_format_mac(char *buf, const unsigned char *addr, int len);
 
 #endif	/* _LINUX_IF_ETHER_H */
