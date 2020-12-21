274e9ecb6c762cd418aad5aac5adcd22005640f3
Neil Jerram <neil@tigera.io>
2020-12-21 12:34:16 +0000

    Add debug facility to 'calico-bpf ipsets dump'

--- cmd/calico-bpf/commands/ipsets.go
+++ cmd/calico-bpf/commands/ipsets.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2020 Tigera, Inc. All rights reserved.
+// Copyright (c) 2019-2021 Tigera, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -56,6 +56,8 @@ func dumpIPSets() error {
 
 	membersBySet := map[uint64][]string{}
 	err := ipsetMap.Iter(func(k, v []byte) bpf.IteratorAction {
+		log.Debugf("K: %v", k)
+		log.Debugf("V: %v", v)
 		var entry ipsets.IPSetEntry
 		copy(entry[:], k[:])
 		var member string
