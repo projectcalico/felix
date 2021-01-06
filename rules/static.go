f6a1265b8f7f9af104c5cd0b4eaa24920f4ea725
Rob Brockbank <rob@tigera.io>
2021-01-05 18:49:27 -0800

    Add RPF use mark

--- rules/static.go
+++ rules/static.go
@@ -838,6 +838,10 @@ func (r *DefaultRuleRenderer) StaticManglePreroutingChain(ipVersion uint8) *Chai
 					RestoreMask: uint32(r.Config.WireguardMarkDoNotRouteViaWireguard + r.Config.WireguardMarkNonCaliWorkloadIface),
 				},
 			},
+			Rule{
+				Match: Match().InInterface(r.Config.WireguardInterfaceName).ConntrackState("NEW").RPFCheckFailed(true, true),
+				Action: DropAction{},
+			},
 		)
 	}
 
@@ -1074,7 +1078,7 @@ func RPFilter(ipVersion uint8, mark, mask uint32, openStackSpecialCasesEnabled, 
 	}
 
 	rules = append(rules, Rule{
-		Match:  Match().MarkMatchesWithMask(mark, mask).RPFCheckFailed(acceptLocal),
+		Match:  Match().MarkMatchesWithMask(mark, mask).RPFCheckFailed(acceptLocal, false),
 		Action: DropAction{},
 	})
 
