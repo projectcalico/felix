7f90808e4d436cc0cea591bddfb4650718472d69
Mike Stephen <mike.stephen@tigera.io>
2021-04-20 15:50:59 +0100

    added firewall rule to mark packets from the wireguard tunnel

--- rules/static.go
+++ rules/static.go
@@ -992,6 +992,17 @@ func (r *DefaultRuleRenderer) StaticRawPreroutingChain(ipVersion uint8) *Chain {
 		Rule{Action: ClearMarkAction{Mark: r.allCalicoMarkBits()}},
 	)
 
+	// Set a mark on the packet if it has come from the WireGuard tunnel to ensure the RPF check allows it
+	// TODO: Mike - need to also check if route src is WorkloadIPs here??
+	if ipVersion == 4 && r.WireguardEnabled && len(r.WireguardInterfaceName) > 0 {
+		rules = append(rules, Rule{
+			Match: Match().Protocol("udp").
+				DestPorts(uint16(r.WireguardListeningPort)).
+				NotSrcAddrType(AddrTypeLocal, false),
+			Action: SetMarkAction{Mark: r.WireguardIptablesMark},
+		})
+	}
+
 	// Set a mark on the packet if it's from a workload interface.
 	markFromWorkload := r.IptablesMarkScratch0
 	for _, ifacePrefix := range r.WorkloadIfacePrefixes {
