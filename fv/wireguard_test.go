0b4fe67976383f967138ad1e46eda48287c7f2a3
Mike Stephen <mike.stephen@tigera.io>
2021-05-11 09:47:28 +0100

    reducing timeout back to 10s

--- fv/wireguard_test.go
+++ fv/wireguard_test.go
@@ -1107,7 +1107,7 @@ var _ = infrastructure.DatastoreDescribe("_BPF-SAFE_ WireGuard-Supported 3-node 
 		By("verifying packets between felix-0 and felix-1 is encrypted")
 		cc.ExpectSome(wlsByHost[0][1], wlsByHost[1][0])
 		cc.ExpectSome(wlsByHost[1][0], wlsByHost[0][1])
-		cc.CheckConnectivityWithTimeout(30 * time.Second)
+		cc.CheckConnectivityWithTimeout(10 * time.Second)
 		for i := range []int{0, 1} {
 			numNonTunnelPacketsFelix0toFelix1Before := tcpdumps[i].MatchCount("numNonTunnelPacketsFelix0toFelix1")
 			numNonTunnelPacketsFelix1toFelix0Before := tcpdumps[i].MatchCount("numNonTunnelPacketsFelix1toFelix0")
@@ -1146,7 +1146,7 @@ var _ = infrastructure.DatastoreDescribe("_BPF-SAFE_ WireGuard-Supported 3-node 
 		By("checking external node to pod connectivity")
 		cc.ExpectSome(externalClient, wlsByHost[0][0])
 
-		cc.CheckConnectivityWithTimeout(30 * time.Second)
+		cc.CheckConnectivityWithTimeout(10 * time.Second)
 	})
 })
 
