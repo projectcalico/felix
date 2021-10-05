7a13e9d807a6f130c084bf8dae2f4906627d9bc8
sridhar <sridhar@tigera.io>
2021-10-04 20:28:39 -0700

    Fix jump maps and ipset maps for xdp

--- bpf-gpl/jump.h
+++ bpf-gpl/jump.h
@@ -37,6 +37,10 @@ struct bpf_map_def_extended __attribute__((section("maps"))) cali_jump = {
 	.key_size = 4,
 	.value_size = 4,
 	.max_entries = 8,
+#if !defined(__BPFTOOL_LOADER__) && defined(__IPTOOL_LOADER__)
+	.map_id = 1,
+	.pinning_strategy = 1 /* object namespace */,
+#endif
 };
 
 /* Add new values to the end as these are program indices */
